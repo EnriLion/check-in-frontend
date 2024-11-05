e33db0e50dbd40ad4c88670ea8d13aa0517f0457
EnriLion <enriqueleon0201leon@gmail.com>
2024-11-05 08:57:14 -0600

    Adding new designs

---
+++ js/location.js
@@ -0,0 +1,115 @@
+const GET_LOCATION = 'http://localhost:8762/location/api/v1/location/records'
+
+////GET all data from location
+fetch(GET_LOCATION).then((data)=> {
+	return data.json();
+}).then((objectData)=>{
+	let tableData="";
+	objectData.map((values)=>{
+		tableData+=`
+			<tr>
+				<td>${values.location}</td>
+				<td>${values.employee.id} - ${values.employee.name}</td>
+				<td>${values.check.checkInId}</td>
+				<td>${values.city}</td>
+				<td>${values.country}</td>
+			</tr>
+		`;
+		document.getElementById("table_location").innerHTML=tableData;
+	});
+})
+
+function findLocationById() {
+    const locationId = document.getElementById('locationIdInput').value;
+    if (locationId) {
+        document.getElementById("table_location").innerHTML = '';
+
+        fetch(`http://localhost:8762/location/api/v1/location/${locationId}/record`)
+            .then(response => response.json())
+            .then(data => {
+                let tableData = ''; 
+
+                if (data.length > 0) {
+                    data.forEach(values => {
+                        tableData += `
+                        <tr>
+			        <td>${values.location}</td>
+		        	<td>${values.employee.id} - ${values.employee.name}</td>
+				<td>${values.check.checkInId}</td>
+				<td>${values.city}</td>
+				<td>${values.country}</td>
+                        </tr>
+                        `;
+                    });
+                } else {
+                    tableData = `
+                    <tr>
+                        <td colspan="6" style='color: red'>There's not LocationID: ${locationId}</td>
+                    </tr>
+                    `;
+                }
+
+                document.getElementById("table_location").innerHTML = tableData;
+            })
+            .catch(error => console.error('Error:', error));
+    } else {
+        alert('Please enter a valid Location ID');
+    }
+}
+
+function findLocations(){
+	fetch(GET_LOCATION).then((data)=>{
+		return data.json();
+	}).then((objectdata)=>{
+		let tableData="";
+		objectdata.map((values)=>{
+		tableData+=`
+                        <tr>
+			        <td>${values.location}</td>
+		        	<td>${values.employee.id} - ${values.employee.name}</td>
+				<td>${values.check.checkInId}</td>
+				<td>${values.city}</td>
+				<td>${values.country}</td>
+                        </tr>
+		 `;
+	});
+	document.getElementById("table_location").innerHTML=tableData;
+	})
+}
+
+function deleteLocationById() {
+    const locationId = document.getElementById('locationIdDelete').value;
+    
+    if (locationId) {
+        // Clear the table data
+        document.getElementById("table_location").innerHTML = '';
+
+        fetch(`http://localhost:8762/location/api/v1/location/${locationId}/delete`, {
+		method: 'DELETE'
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();  
+        })
+        .then(data => {
+            let tableData = `
+            <tr>
+                <td colspan="6" style='color: red'>Location ID:  ${locationId} has been deleted.</td>
+            </tr>`;
+            
+            document.getElementById("table_location").innerHTML = tableData;
+        })
+        .catch(error => {
+            let tableData = `
+            <tr>
+                <td colspan="6" style='color: red'>There's not Location ID ${locationId} </td>
+            </tr>`;
+            
+            document.getElementById("table_location").innerHTML = tableData;
+        });
+    } else {
+        alert('Please enter a valid Employee ID');
+    }
+}
